95443f4112c0537f91b2401e5d3d6db36e4229eb
vardan-info <69822867+vardan-info@users.noreply.github.com>
2020-08-17 15:13:36 -0700

    Add files via upload

---
+++ lesson3/form.php
@@ -0,0 +1,34 @@
+<?php
+if (mkdir("newfolder"))
+   echo "Папка успешно создана";
+else
+   echo "Папка не создана";
+?>
+
+<?php include 'index.php';?>
+<?php
+if( isset($_POST['n']) && isset($_POST['e']) && isset($_POST['m']) ){
+	$n = $_POST['n']; // HINT: use preg_replace() to filter the data
+	$e = $_POST['e'];
+	$m = nl2br($_POST['m']);
+	$to = "you@domain.com";	
+	$from = $e;
+	$subject = 'Contact Form Message';
+	$message = '<b>Name:</b> '.$n.' <br><b>Email:</b> '.$e.' <p>'.$m.'</p>';
+	$headers = "From: $from\n";
+	$headers .= "MIME-Version: 1.0\n";
+	$headers .= "Content-type: text/html; charset=iso-8859-1\n";
+	if( mail($to, $subject, $message, $headers) ){
+		echo "success";
+	} else {
+		echo "The server failed to send the message. Please try again later.";
+	}
+}
+?>
+
+<?php
+// Set session variables
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+echo "Session variables are set.";
+?>
